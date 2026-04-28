928cbfa1db54db4dcb87e33f8d0c4ddbc7d3cd5f
sheeuzbpro-star <sheeuzbpro@gmail.com>
2026-04-28 08:47:20 +0500

    index.js

--- index.js
+++ index.js
@@ -253,4 +253,5 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.listen(PORT, () => console.log(\`Server \${PORT}-portda ishladi!\`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server ishladi!"));
